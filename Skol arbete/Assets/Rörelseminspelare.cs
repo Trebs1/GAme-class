be04e6fb5ac7ab62c2e31c182fa3100208b229ac
Trebs1 <Vestin.emil@gmail.com>
2023-03-16 16:03:32 +0100

    Spring animation

--- Skol arbete/Assets/Rörelseminspelare.cs
+++ Skol arbete/Assets/Rörelseminspelare.cs
@@ -1,23 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 
 public class Rörelseminspelare : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private Animator animator;
+    private float motX;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-       
+       rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
 ;   }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if (Input.GetKeyDown("space"))
+        motX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(motX * 7f, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump"))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 14, 0);
+            rb.velocity = new Vector3(rb.velocity.x, 14f);
+        }
+        
+        UpdateAnimationUpdate();
+    }
+    private void UpdateAnimationUpdate()
+    {
+        if (motX > 0f)
+        {
+            animator.SetBool("springer", true);
+        }
+        else if (motX < 0f)
+        {
+            animator.SetBool("springer", true);
+        }
+        else
+        {
+            animator.SetBool("springer", false);
         }
     }
 
