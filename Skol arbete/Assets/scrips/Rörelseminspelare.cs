e8209cdee24e06ef4f9313f116f66b7ee0924721
Trebs1 <Vestin.emil@gmail.com>
2023-04-20 22:00:49 +0200

    mycke

--- Skol arbete/Assets/scrips/Rörelseminspelare.cs
+++ Skol arbete/Assets/scrips/Rörelseminspelare.cs
@@ -6,21 +6,26 @@ using UnityEngine;
 public class Rörelseminspelare : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private BoxCollider2D koll;
     private Animator animator;
+    [SerializeField] private LayerMask Hoppmark;
     private SpriteRenderer sprite;
     private float motX = 0f;
-    [SerializeField]private float springFart = 7f;
-    [SerializeField]private float hoppkraft = 14f;
-    
+    [SerializeField] private float springFart = 7f;
+    [SerializeField] private float hoppkraft = 14f;
+    private enum Rörelsedelar { rörelse, springa, hoppa, falla }
+
 
     // Start is called before the first frame update
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        koll = GetComponent<BoxCollider2D>();
         sprite = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
 
-;   }
+        ;
+    }
 
     // Update is called once per frame
     private void Update()
@@ -28,30 +33,48 @@ public class Rörelseminspelare : MonoBehaviour
         motX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(motX * springFart, rb.velocity.y);
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && Landat())
         {
             rb.velocity = new Vector3(rb.velocity.x, hoppkraft);
         }
-        
+
         UpdateAnimationState();
     }
     private void UpdateAnimationState()
     {
+        Rörelsedelar state;
+
         if (motX > 0f)
         {
-            animator.SetBool("springer", true);
+            state = Rörelsedelar.springa;
             sprite.flipX = false;
         }
         else if (motX < 0f)
         {
-            animator.SetBool("springer", true);
-            sprite.flipX=true;
-            
+            state = Rörelsedelar.springa;
+            sprite.flipX = true;
+
         }
         else
         {
-            animator.SetBool("springer", false);
+            state = Rörelsedelar.rörelse;
         }
+
+        if (rb.velocity.y > .1f)
+        {
+            state = Rörelsedelar.hoppa;
+        }
+        else if (rb.velocity.y < -.1f)
+        {
+            state = Rörelsedelar.falla;
+        }
+
+        animator.SetInteger("state", (int)state);
     }
 
+    private bool Landat()
+    {
+        return Physics2D.BoxCast(koll.bounds.center, koll.bounds.size, 0f, Vector2.down, .1f, Hoppmark);
+    }
 }
+
