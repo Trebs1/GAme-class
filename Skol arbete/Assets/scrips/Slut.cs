61bb899f69247f809f6fd0182375b484e688c2f0
Trebs1 <Vestin.emil@gmail.com>
2023-05-14 21:14:10 +0200

    Quit

---
+++ Skol arbete/Assets/scrips/Slut.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slut : MonoBehaviour
+{
+    public void lamna()
+    {
+        Application.Quit();
+    }
+}
